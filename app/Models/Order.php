51ef2b55919509cdcde8d86532b7b1c9944f3b8f
rmkuro <another1112a@gmail.com>
2022-10-17 23:01:37 +0900

    [バリデーションの追加]item_idのバリデーションを追加

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Item;
 
 class Order extends Model
 {
     use HasFactory;
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
 }
