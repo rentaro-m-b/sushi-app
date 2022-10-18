0833920193f7cc68444cf2ee0e93db7d270f75f4
rmkuro <another1112a@gmail.com>
2022-10-18 15:39:39 +0900

    [修正]余計なコメントアウトの削除

--- app/UseCases/Order/Create.php
+++ app/UseCases/Order/Create.php
@@ -14,6 +14,8 @@ class Create{
             return false;
         }
         
+        //商品(item)が、寿司カテゴリでない、かつvolumeが付与されている場合にエラーを返す。(後で実装)
+        //ItemOption::find($order['item_id'])->category_id;
         foreach($orders as $order){
             Order::create([
                 'item_id' => $order['item_id'],
@@ -23,26 +25,5 @@ class Create{
         }
 
         return true;
-
-        // foreach($orders as $order){
-        //     //orderは配列(連想配列)
-        //     //注文にoptionsが含まれているかを確認
-        //     if(array_key_exists('options', $order)){
-        //         foreach($order['options'] as $option){
-        //             $result = ItemOption::where('item_id', $order['item_id'])->where('option_id', $option)->exists();
-        //             if(!$result){
-        //                 return response("不適切なオプションです");
-        //             }
-        //         }
-        //     };
-        
-        //     //商品(item)が、寿司カテゴリでない、かつvolumeが付与されている場合にエラーを返す。(後で実装)
-        //     //ItemOption::find($order['item_id'])->category_id;
-        //     Order::create([
-        //         'item_id' => $order['item_id'],
-        //         'table_id' => $table_id,
-        //         'paid' => false
-        //     ]);
-        // }
     }
 }
