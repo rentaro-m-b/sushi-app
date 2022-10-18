8ed8e71ffeb54297d5f943a9440b5a873a4df515
rmkuro <another1112a@gmail.com>
2022-10-18 14:27:03 +0900

    /ordersの修正

--- app/Models/ItemOption.php
+++ app/Models/ItemOption.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Item;
 use App\Models\Option;
+use App\Models\ItemOption;
 
 class ItemOption extends Model
 {
@@ -19,5 +20,19 @@ class ItemOption extends Model
     public function option()
     {
         return $this->belongsTo(Option::class);
+    }
+
+    public static function check_options(array $orders){
+        foreach($orders as $order){
+            if(array_key_exists('options', $order)){
+                foreach($order['options'] as $option){
+                    $result = ItemOption::where('item_id', $order['item_id'])->where('option_id', $option)->exists();
+                    if(!$result){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
