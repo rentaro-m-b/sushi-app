25d08e762397b0ceb4d1a169567a1e9c3c2935de
Rentaro Mori <re_mori@yumemi.co.jp>
2022-10-24 10:01:19 +0900

    マイグレーションファイルの無名クラスの変更＋作成日時、更新日時カラムの追加

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -9,8 +9,6 @@ class Item extends Model
 {
     use HasFactory;
 
-    public $timestamps = false;
-
     protected $fillable = [
         'name',
         'price',
--- database/migrations/2022_10_18_030244_create_items_table.php
+++ database/migrations/2022_10_18_030244_create_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateItemTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -20,6 +20,7 @@ class CreateItemTable extends Migration
             $table->integer('stock');
             $table->foreignId('category_id')->constrained('categories');
             $table->boolean('on_sale');
+            $table->timestamps();
         });
     }
 
